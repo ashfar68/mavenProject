580e84c356fce933498c4f730749bbfb84c20405
Ashfar <drashfaralammalik@gmail.com>
2021-12-24 02:20:10 +0000

    initial commit

---
+++ src/test/java/HelloTest.java
@@ -0,0 +1,8 @@
+public class HelloTest {
+
+    public static void main(String[] args) {
+        System.out.println("HelloTest");
+        System.out.println("where is the mistake");
+        System.out.println("new way to do");
+    }
+}
